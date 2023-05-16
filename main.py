fa4efb1962391eb71ef7e991aea5ed25ac875ecd
John Bjorge <johnedwardbjorge@gmail.com>
2023-05-15 22:02:43 -0700

    wip: simplification of main.py

--- main.py
+++ main.py
@@ -5,22 +5,16 @@ import json
 from data_aggregator import DataAggregator
 
 
-def run_scraper(date_search, data_queue):
+def run_scraper(date_search):
     pgs = PremierGolfScraper(date_search=date_search, course_search="Jackson Park")
+    pgs.start_driver()
     html = pgs.navigate_page()
-    pgs.scrape(html)
-    results = pgs.get_tee_time_search_results()
+    tee_times = pgs.scrape(html)
+    results = pgs.get_tee_time_search_results(tee_times)
     pgs.save_to_json(results)
     pgs.quit()
 
-def get_dates_range(start_day: str = date.today(), end_day: str = date.today() + timedelta(14)) -> list:
-    if start_day < date.today():
-        raise ValueError(f"start_day of {start_day} must be greater than or equal to today")
-    if end_day > date.today() + timedelta(14):
-        raise ValueError(f"end_day of {end_day} must be less than two weeks out from today")
-    if start_day > end_day:
-        raise ValueError(f"start_day of {start_day} must be less than end_day of {end_day}")
-
+def get_dates_range(start_day: date = date.today(), end_day: date = date.today() + timedelta(14)) -> list:
     dates_list = []
     day_range = end_day - start_day
     for day in range(day_range.days):
@@ -32,15 +26,14 @@ if __name__ == "__main__":
     end_day = start_day + timedelta(days=2)
     dates_list = get_dates_range(start_day, end_day)
 
-
     # consider using threads instead of processes
 
     # create a list to hold the processes
     processes = []
 
     # start a new process for each URL
-    for date in dates_list:
-        process = Process(target=run_scraper, args=(date, data_queue))
+    for day in dates_list:
+        process = Process(target=run_scraper, args=(day,))
         process.start()
         processes.append(process)
 
@@ -51,7 +44,8 @@ if __name__ == "__main__":
     data_aggregator = DataAggregator()
     # for file in files that I generated in sample directory
     for _ in range(len(dates_list)):
-        data_aggregator.add_data(data_queue.get())
+        print("es")
+        # data_aggregator.add_data(data_queue.get())
 
     # call data_aggregator.save_data()
 
