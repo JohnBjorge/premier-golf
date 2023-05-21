b5d5cc98c7e2147fafcb40de29fe80919925b2f9
John Bjorge <johnedwardbjorge@gmail.com>
2023-05-20 21:56:29 -0700

    split out aggregate data and upload data into two tasks

--- main.py
+++ main.py
@@ -24,23 +24,28 @@ def threaded_scrape(dates_list, max_workers):
             executor.submit(run_scraper, day)
 
 @task()
-def aggregate_data():
-    data_aggregator = DataAggregator()
+def aggregate_data(data_aggregator):
     data_aggregator.aggregate_data()
     data_aggregator.save_data()
+
+@task()
+def upload_to_gcs(data_aggregator):
     data_aggregator.upload_to_google()
+
 
 @flow(task_runner=SequentialTaskRunner())
 def scrape_to_gcs(dates_list, max_workers):
     threaded_scrape.submit(dates_list, max_workers)
-    aggregate_data.submit()
+    data_aggregator = DataAggregator()
+    aggregate_data.submit(data_aggregator)
+    upload_to_gcs.submit(data_aggregator)
 
 if __name__ == "__main__":
     start_date = date.today()
-    number_of_days = 1
+    number_of_days = 2
 
     dates_list = [start_date + timedelta(days=i) for i in range(number_of_days)]
-    
+
     max_workers = 6
     scrape_to_gcs(dates_list, max_workers)
 
